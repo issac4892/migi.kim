eabb5e647f53231dad4d5029fe3297cd921d225d
issac4892 <issac4892@gmail.com>
2023-06-14 11:58:33 +0900

    color revamp

--- src/Email.tsx
+++ src/Email.tsx
@@ -4,11 +4,13 @@ export default function Email() {
     const route = useNavigate()
     return (
         <div className="page font-suit">
-            <h1 className="suite"><span className="back" onClick={() => route('/')}>←</span> Email</h1>
+            <h1 className="suite">
+                <span className="back" onClick={() => route('/')}>← </span>
+                <span className="tossface">✉️</span>Email</h1>
             <div className="pagedata email-div">
-                Work | <Link className="emaillink" to="mailto:mingeon.kim@algorix.io">mingeon.kim@algorix.io</Link><br/>
-                Personal | <Link className="emaillink" to="mailto:issac4892@gmail.com">issac4892@gmail.com</Link><br/>
-                Personal | <Link className="emaillink" to="mailto:me@migi.kim">me@migi.kim</Link>
+                <span className="tossface">🏢️</span>Work | <Link className="emaillink" to="mailto:mingeon.kim@algorix.io">mingeon.kim@algorix.io</Link><br/>
+                <span className="tossface">👨‍💻</span>Personal | <Link className="emaillink" to="mailto:issac4892@gmail.com">issac4892@gmail.com</Link><br/>
+                <span className="tossface">👨‍💻</span>Personal | <Link className="emaillink" to="mailto:me@migi.kim">me@migi.kim</Link>
             </div>
         </div>
     )
