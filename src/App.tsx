5e0f2028857ebe2ffcf0dfb04cc5a9114685cc46
issac4892 <issac4892@gmail.com>
2023-06-14 15:47:22 +0900

    title fix - main page

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,43 @@ import GithubIcon from "./icons/github";
 import LinkedInIcon from "./icons/linkedin";
 import DiscordIcon from "./icons/discord";
 import {Link, useNavigate} from "react-router-dom";
-import { Tooltip } from 'react-tooltip'
+import {Tooltip} from 'react-tooltip'
+import Helmet from "react-helmet";
 
 function App() {
     const route = useNavigate()
     return (
-        <div className="page main">
-            <Tooltip id="discord"/>
-            <img src="pfp.png" className="profile-pic" alt="Profile Picture"/><br/>
-            <span className="font-suit font-big">Mingeon Kim</span><br/>
-            <span>Fullstack Developer</span>
-            <div className="flex-icon">
-                <div onClick={() => route('/email')}>
-                    <EmailIcon/>
+        <>
+            <Helmet>
+                <title>Mingeon Kim</title>
+            </Helmet>
+            <div className="page main">
+                <Tooltip id="discord"/>
+                <img src="pfp.png" className="profile-pic" alt="Profile Picture"/><br/>
+                <span className="font-suit font-big">Mingeon Kim</span><br/>
+                <span>Fullstack Developer</span>
+                <div className="flex-icon">
+                    <div onClick={() => route('/email')}>
+                        <EmailIcon/>
+                    </div>
+                    <div onClick={() => window.location.href = 'https://github.com/issac4892'}>
+                        <GithubIcon/>
+                    </div>
+                    <div onClick={() => window.location.href = 'https://www.linkedin.com/in/mingeonkim'}>
+                        <LinkedInIcon/>
+                    </div>
+                    <div data-tooltip-id="discord" data-tooltip-content="migi" data-tooltip-place="bottom">
+                        <DiscordIcon/>
+                    </div>
                 </div>
-                <div onClick={() => window.location.href = 'https://github.com/issac4892'}>
-                    <GithubIcon/>
+                <div className="flex-icon" style={{width: '200px'}}>
+                    <Link to="/career" className="link"><span className="tossface">💻</span> Career</Link>
+                    <Link to="/projects" className="link"><span className="tossface">🛠</span>️ Projects</Link>
+                    <Link to="/education" className="link"><span className="tossface">🏫</span> Education</Link>
                 </div>
-                <div onClick={() => window.location.href = 'https://www.linkedin.com/in/mingeonkim'}>
-                    <LinkedInIcon/>
-                </div>
-                <div data-tooltip-id="discord" data-tooltip-content="migi" data-tooltip-place="bottom">
-                    <DiscordIcon/>
-                </div>
+                <span className="font-suit font-small">Mingeon Kim @ 2023</span>
             </div>
-            <div className="flex-icon" style={{ width: '200px' }}>
-                <Link to="/career" className="link"><span className="tossface">💻</span> Career</Link>
-                <Link to="/projects" className="link"><span className="tossface">🛠</span>️ Projects</Link>
-                <Link to="/education" className="link"><span className="tossface">🏫</span> Education</Link>
-            </div>
-            <span className="font-suit font-small">Mingeon Kim @ 2023</span>
-        </div>
+        </>
     )
 }
 
