f3b4b3072f80f2563ff02e2cf07488860fd18b0a
issac4892 <issac4892@gmail.com>
2023-07-03 18:43:59 +0900

    Update Email.tsx

--- src/Email.tsx
+++ src/Email.tsx
@@ -14,8 +14,8 @@ export default function Email() {
                 <span className="tossface">✉️</span>Email</h1>
             <div className="pagedata email-div">
                 <span className="tossface">🏢️</span>Work | <Link className="emaillink" to="mailto:mingeon.kim@algorix.io">mingeon.kim@algorix.io</Link><br/>
-                <span className="tossface">👨‍💻</span>Personal | <Link className="emaillink" to="mailto:issac4892@gmail.com">issac4892@gmail.com</Link><br/>
-                <span className="tossface">👨‍💻</span>Personal | <Link className="emaillink" to="mailto:me@migi.kim">me@migi.kim</Link>
+                <span className="tossface">👤</span>Personal | <Link className="emaillink" to="mailto:issac4892@gmail.com">issac4892@gmail.com</Link><br/>
+                <span className="tossface">👤</span>Personal | <Link className="emaillink" to="mailto:me@migi.kim">me@migi.kim</Link>
             </div>
         </div>
             </>
